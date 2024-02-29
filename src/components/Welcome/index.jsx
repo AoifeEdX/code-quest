62b9729f6659df92cc854aa029df4f1e9d255253
hszilvi <szilvi@naszta.hu>
2024-02-29 11:46:39 +0000

    create welcome component basics

---
+++ src/components/Welcome/index.jsx
@@ -0,0 +1,23 @@
+const Welcome = () => {
+    return (
+        <>
+         <div className='d-flex flex-column flex-md-column flex-lg-row mt-3'>
+                <div className='p-5 text-left text-content'>
+                    <h1 className='mt-5 mb-3 text-left'>Welcome to CodeQuest: Front-end Chronicles!</h1>
+                    <h4 className='mb-3 fw-bold h3 text-left'>Ay you embark on your journey, you'll be greeted with an immersive introduction to the digital realm of CodeQuest.</h4>
+                    <form>
+
+                    </form>
+                    <p className='mt-5 h4'>Meet [name], the skilled front-end developer, and learn about the challanges plaguing the digital kingdom.</p>
+                </div>
+                <div className='hero-img'>
+                    
+                </div>
+            </div>    
+        
+        
+        </>
+    )
+}
+
+export default Welcome;
