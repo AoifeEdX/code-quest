c496030d0e092c36d8619f4bc3cf42c2764f3a1b
Adebayo <adenguk5251@gmail.com>
2024-03-06 21:28:03 +0000

    changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import {  GitHub, Instagram, LinkedIn } from '@mui/icons-material';
 
 
 
-function Footer() {
+  function Footer() {
   const [feedback,setFeedback] = useState(false)
   const [email,setEmail] = useState({Email:""})
   const scriptURL = 'https://script.google.com/macros/s/AKfycbwjBpRhdUIk8hhbE6PK9idYs9c6vU8KEF-awsYR-7yFCcOzim4MSA9cuB9WJBjPKHTJ/exec'
@@ -41,7 +41,7 @@ function Footer() {
           <div className="d-flex">
             <form onSubmit= {handleSubmit}>
             <input id="email" name="Email" value={email.Email} onChange= {handleChange} type="email" className="form-control me-2 rounded-pill" placeholder="Your email address" />
-            <Button type="submit" className="btn rounded-pill gradient-bg-blue text-white ms-1">Subscribe</Button>
+            <Button type="submit" className="btn rounded-pill gradient-bg-blue text-white ms-1"> Subscribe </Button>
             </form>
             <span > {feedback? "Thank you for subscribing":""}</span>
           </div>
