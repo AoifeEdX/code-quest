831be1ec78b36d2427ab2510e9b4f762b685a619
hszilvi <szilvi@naszta.hu>
2024-03-07 10:06:17 +0000

    fix warning on htmllevel

--- src/components/HtmlChallenge/HtlmLives.jsx
+++ src/components/HtmlChallenge/HtlmLives.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const HtmlLives = ({ displayedLives }) => {
-	const hearts = Array.from({ length: displayedLives }, (index) => (
+	const fires = new Array(displayedLives).fill(null).map(_, index) => (
 		<span key={index}>🔥</span>
 	));
 
 	return (
 		<div className="d-flex justify-content-between text-danger fs-4 fw-bold bangers-text">
 			<p>Lives:</p>
-			<p className="text-end">{hearts}</p>
+			<p className="text-end">{fires}</p>
 		</div>
 
 	);
