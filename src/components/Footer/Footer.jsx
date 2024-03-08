2e652a7c5074b16df8e13f153f40a38310d878d2
AoifeEdX <ayohwife@gmail.com>
2024-03-08 10:41:31 +0000

    Added GitHub Explorer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,87 +1,103 @@
 import React, { useState } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import IconButton from '@mui/material/IconButton';
-import {  GitHub, Instagram, LinkedIn } from '@mui/icons-material';
+import { GitHub, Instagram, LinkedIn } from '@mui/icons-material';
+import logo from '/images/logo.png';
 
+function Footer() {
+	const [feedback, setFeedback] = useState(false);
+	const [email, setEmail] = useState("");
+	const [validEmail, setValidEmail] = useState(true);
+	const [errorMessage, setErrorMessage] = useState("");
 
+	const scriptURL = 'https://script.google.com/macros/s/AKfycbwjBpRhdUIk8hhbE6PK9idYs9c6vU8KEF-awsYR-7yFCcOzim4MSA9cuB9WJBjPKHTJ/exec';
 
-  function Footer() {
-  const [feedback,setFeedback] = useState(false)
-  const [email,setEmail] = useState({Email:""})
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbwjBpRhdUIk8hhbE6PK9idYs9c6vU8KEF-awsYR-7yFCcOzim4MSA9cuB9WJBjPKHTJ/exec'
-  
-   const handleChange = (e) => {
-    const {name, value} = e.target
-    setEmail ({[name]:value})
-   }
-  const handleSubmit = (e) =>{
-    e.preventDefault ();
-    console.log(e)
-    fetch(scriptURL, {method:'POST',contentType: 'application/json', body: JSON.stringify(email) })
+	const handleChange = (e) => {
+		const value = e.target.value;
+		setEmail(value);
+		setValidEmail(validateEmail(value));
+	};
 
-          .then(response=> {return response.json()}
-            
-          ).then(data=>{console.log(data)
-            setFeedback(true)
-            setTimeout(function(){setFeedback(false)},10000)
-            setEmail({Email:""})
-          })
-          .catch(error => console.error('Error!', error.message))
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (validEmail) {
+			fetch(scriptURL, { method: 'POST', contentType: 'application/json', body: JSON.stringify({ Email: email }) })
+				.then(response => response.json())
+				.then(data => {
+					console.log(data);
+					setFeedback(true);
+					setTimeout(() => setFeedback(false), 10000);
+					setEmail("");
+				})
+				.catch(error => {
+					setErrorMessage("Failed to submit. Please try again later.");
+					console.error('Error!', error.message);
+				});
+		} else {
+			setErrorMessage("Please enter a valid email address.");
+		}
+	};
 
-  }
- 
+	const validateEmail = (email) => {
+		// Added this regex for basic email validation
+		const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		return regex.test(email);
+	};
 
-  return (
-    <Container className="py-5">
+	return (
+		<Container className="py-5">
 			<hr className="my-5"></hr>
-      <Row>        
-			<Col xs={12} md={4} className="mb-5 text-md-start text-center">
-          <h5 className="mb-3">Newsletter</h5>
-          <p className="fw-light">Get weekly updates on our game releases</p>
-          <div className="d-flex">
-            <form onSubmit= {handleSubmit}>
-            <input id="email" name="Email" value={email.Email} onChange= {handleChange} type="email" className="form-control me-2 rounded-pill" placeholder="Your email address"  pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" required/>
-            <Button type="submit" className="btn rounded-pill gradient-bg-blue text-white ms-1"> Subscribe </Button>
-            </form>
-            <span > {feedback? "Thank you for subscribing":""}</span>
-          </div>
-        </Col>
+			<Row>
+				<Col xs={12} md={4} className="mb-5 text-md-start text-center">
+					<h5 className="mb-3">Newsletter</h5>
+					<p className="fw-light">Get weekly updates on our game releases</p>
+					<form onSubmit={handleSubmit}>
+						<div className="d-flex">
+							<input
+								type="email"
+								className={`form-control me-2 rounded-pill ${validEmail ? '' : 'is-invalid'}`}
+								value={email}
+								onChange={handleChange}
+								placeholder="Your email address"
+								required
+							/>
+							<Button type="submit" className="btn rounded-pill gradient-bg-blue text-white ms-1">Subscribe</Button>
+						</div>
+						{!validEmail && <div className="invalid-feedback">{errorMessage}</div>}
+					</form>
+				</Col>
 				<Col xs={12} md={3} className="text-center mb-5">
-          <h5 className="mb-3">Information</h5>
-          <ul className="list-unstyled">						
-					<li>
-              <a href="/about" className="fw-light link-light link-offset-3 link-underline-opacity-25 link-underline-opacity-100-hover">About us</a>
-            </li>
-            <li className="my-3">
-              <a href="/contact" className="fw-light link-light link-offset-3 link-underline-opacity-25 link-underline-opacity-100-hover">Contact us</a>
-            </li>
-          </ul>
-        </Col>
+					<h5 className="mb-3">Information</h5>
+					<ul className="list-unstyled">
+						<li>
+							<a href="/about" className="fw-light link-light link-offset-3 link-underline-opacity-25 link-underline-opacity-100-hover">About us</a>
+						</li>
+						<li className="my-3">
+							<a href="/contact" className="fw-light link-light link-offset-3 link-underline-opacity-25 link-underline-opacity-100-hover">Contact us</a>
+						</li>
+					</ul>
+				</Col>
 				<Col xs={12} md={3} className="text-center mb-5">
-          <h5 className="mb-3">Follow Us</h5>
-          <div>
-            <IconButton color="inherit" href="https://github.com/AoifeEdX/code-quest" target="_blank" aria-label="GitHub" className="text-white me-2">
-              <GitHub />
-            </IconButton>
-            <IconButton color="inherit" href="https://instagram.com" target="_blank" aria-label="Instagram" className="text-white me-2">
-              <Instagram />
-            </IconButton>
-            <IconButton color="inherit" href="https://www.linkedin.com" target="_blank" aria-label="LinkedIn" className="text-white">
-              <LinkedIn />
-            </IconButton>
-          </div>
-        </Col>
+					<h5 className="mb-3">Follow Us</h5>
+					<div>
+						<IconButton color="inherit" href="https://github.com/AoifeEdX/code-quest" target="_blank" aria-label="GitHub" className="text-white me-2">
+							<GitHub />
+						</IconButton>
+						<IconButton color="inherit" href="https://instagram.com" target="_blank" aria-label="Instagram" className="text-white me-2">
+							<Instagram />
+						</IconButton>
+						<IconButton color="inherit" href="https://www.linkedin.com" target="_blank" aria-label="LinkedIn" className="text-white">
+							<LinkedIn />
+						</IconButton>
+					</div>
+				</Col>
 				<Col xs={12} md={2} className="mb-4 text-center">
-			<p role="img" aria-label="logo" className="fs-2">⚔️</p>
-			<p className="fw-light">© CodeQuest {new Date().getFullYear()}</p>					
-        </Col>
-      </Row>      
-    </Container>
-  );
+					<img src={logo} alt="Logo" className="footer-logo" style={{ maxWidth: '100px', maxHeight: '100px' }} />
+					<p className="fw-light">© {new Date().getFullYear()}</p>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
-
-
-
-
 
 export default Footer;
---
+++ src/pages/Resources/GitHubExplorer.jsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Row, Col, Form, Button, Image } from 'react-bootstrap';
+import RenderRepo from './RenderRepo.jsx';
+import octocat from '/images/octocat.png';
+import officeKnight from '/images/officeKnight.gif';
+
+export default function GitHubExplorer() {
+  const [languages, setLanguages] = useState([]);
+  const [selectedLanguage, setSelectedLanguage] = useState('');
+  const [keyword, setKeyword] = useState('');
+  const [repositories, setRepositories] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    async function fetchLanguages() {
+      try {
+        const response = await axios.get('https://api.github.com/languages');
+				console.log('Languages response:', response.data); // Add this line to log the response data
+
+        //! Uncomment to limit to specific languages
+				// setLanguages(response.data.filter(lang => allowedLanguages.includes(lang.name)));
+        setLanguages(response.data); // Remove filtering
+      } catch (error) {
+        setError('Error fetching languages: ' + error.message);
+      }
+    }
+
+    fetchLanguages();
+  }, []);
+
+  //! Uncomment to limit to specific languages:
+  // const allowedLanguages = ['HTML', 'CSS', 'Java', 'JavaScript', 'React', 'Ruby', 'PHP', 'Python'];
+
+	const exploreRepos = async () => {
+		try {
+			const apiUrl = `https://api.github.com/search/repositories?q=language:${selectedLanguage}+${keyword}&sort=stars&order=desc`;
+			const response = await axios.get(apiUrl);
+			console.log('Repository data:', response.data.items[0]); // Logging data for the first repository
+			setRepositories(response.data.items.slice(0, 20));
+		} catch (error) {
+			setError('Error fetching repositories: ' + error.message);
+		}
+	};
+	
+	return (
+		<Container>
+			<Row>
+				{/* GitHub Icon */}
+				<Col md={6}>
+				<Image src={officeKnight}  alt="GitHub Icon" className="img-fluid p-5" />
+				</Col>
+				<Col md={6} className="d-flex flex-column justify-content-center align-items-center mt-5">
+					<h2>Explore <span className="text-warning">GitHub repositories</span> in your programming language
+						of&nbsp;choice.</h2>
+						<p className="fs-5 my-3 py-3">
+						<span className="text-warning">GitHub Explorer</span> allows you to explore the most popular GitHub
+						repositories based on the programming languages used.
+					</p>
+				</Col>
+			</Row>
+			<Row>
+				<Col md={9} className="my-3 d-flex flex-column justify-content-center">
+					<p className="fs-5">
+						Select a <strong>language</strong>, enter <strong>keywords</strong> if you are looking for code related to a
+						specific feature or topic, and find relevant repositories with ease.
+					</p>
+					<p className="fs-5">
+						The <strong>top 20 repositories</strong> will be listed in order of their star ranking.
+					</p>
+				</Col>
+				<Col md={3} className="d-flex flex-column justify-content-center align-items-center p-5">
+					<Image src={octocat} alt="Knight working at a computer desk" className="img-fluid align-items-center" />
+				</Col>
+			</Row>
+
+			{/* Language Selection and Keyword Search */}
+			<Row className="mb-4">
+				<h3 className="my-2 py-4">Explore GitHub Repositories:</h3>
+				{/* Language Selection Column */}
+				<Col md={6}>
+					<Form.Group>
+						<Form.Label className="fs-5">Select Language:</Form.Label>
+						<Form.Select id="languageSelect" onChange={(e) => setSelectedLanguage(e.target.value)}>
+							<option value="">Select a language</option>
+							{languages.map(language => (
+								<option key={language.name} value={language.name}>{language.name}</option>
+							))}
+						</Form.Select>
+					</Form.Group>
+				</Col>
+
+				{/* Keyword Search Column */}
+				<Col md={6}>
+					<Form.Group>
+						<Form.Label className="fs-5">Keyword Search (optional):</Form.Label>
+						<Form.Control type="text" id="keywordSearch" placeholder="Enter a keyword" onChange={(e) => setKeyword(e.target.value)} />
+					</Form.Group>
+				</Col>
+			</Row>
+
+			{/* Explore Button */}
+			<Button className="btn gradient-bg-orange rounded-pill px-4 fs-5" onClick={exploreRepos}>Explore</Button>
+
+			{/* Error Message */}
+			{error && <div className="my-3 py-3 text-danger">{error}</div>}
+
+			{/* Repository List */}
+			<div id="repoList" className="mt-4">
+				{repositories.map((repo, index) => (
+					index % 2 === 0 && (
+						<Row key={repo.id}>
+								<RenderRepo repo={repo} />
+							{repositories[index + 1] && (
+									<RenderRepo repo={repositories[index + 1]} />
+							)}
+						</Row>
+					)
+				))}
+			</div>
+		</Container>
+	);
+}
+
